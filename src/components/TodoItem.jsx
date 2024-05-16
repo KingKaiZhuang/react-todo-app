73b13f9f573beda6d5059dfa038e9088fb11ebb8
KingKaiZhuang <5b0g0024@stust.edu.tw>
2024-05-16 14:45:26 +0800

    chore: Update TodoItem component and styles
    - Update TodoItem component to toggle item completion status on click
    - Add CSS class for completed items in TodoItem styles
    
    Note: This commit message follows the conventional format of "chore: <description>".

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,22 @@
-import "../assets/scss/todoitem.scss";
+import "../assets/scss/todoitem.scss"; // 使用常規導入
+
 export default function TodoItem({ item, todos, settodos }) {
   function deleteItem(item) {
     console.log("delete item", item);
     settodos(todos.filter((todo) => todo !== item));
   }
+  function handleClick(item) {
+    const newArra = todos.map((todo) => {
+      return todo.name === item.name ? { ...todo, done: !todo.done } : todo;
+    });
+    settodos(newArra);
+  }
+  const className = item.done ? "completed" : ""; // 直接使用CSS類名
   return (
     <div className="item">
-      <h3 className="item-name">{item.name}</h3>
+      <h3 onClick={() => handleClick(item)} className={className}>
+        {item.name}
+      </h3>
       <button onClick={() => deleteItem(item)} className="item-delete">
         X
       </button>
